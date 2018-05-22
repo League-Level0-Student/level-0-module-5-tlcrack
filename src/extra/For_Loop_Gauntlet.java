aa2ef2715213819d33b4f70049fcc3f1e3c3b25e
tlcrack <league@WorthyMini-5.attlocal.net>
2018-05-21 18:28:53 -0700

    for_loop_gauntlet

--- src/extra/For_Loop_Gauntlet.java
+++ src/extra/For_Loop_Gauntlet.java
@@ -8,13 +8,35 @@ public static void main(String[] args) {
 	//2:		for(int b = 100; b > 0; b--) {
 	//System.out.println(b);}
 	
-	//3:		for(int c = 2; c < 100;c++) {
+	//3.		for(int c = 2; c < 100;c++) {
 	//if(c%2==0) {
 	//System.out.println(c);}}
 	
+	//4: for(int d = 0; d < 100; d++) {
+	//if(d%2==1) {
+	//System.out.println(d);}}
 	
+	//5.		for(int d = 1; d < 500; d++) {
+	//if(d%2==1) {
+	//System.out.println(d + " is odd");}
+	//if(d%2==0) {
+	//System.out.println(d + " is even");}}
 	
+	//6.		for (int e = 0; e < 777; e++) {
+	//if(e%7==0) {
+	//System.out.println(e);}}
+		
 	
+	//7.		for (int f = 2007; f < 2018; f++) {
+	//int g = f-2007;
+	//System.out.println("In " + f + " I was " + g);}
+		
 	
+	//1.		
+	//for(int h = 0; h  )
+
+
+
+
 }
 }
