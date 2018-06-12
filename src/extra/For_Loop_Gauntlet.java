1b8c7cd4a771dc8aae45294342dde98023671a35
League <league@WTS14-4.attlocal.net>
2018-06-11 18:26:12 -0700

    SkillPractice
    finally

--- src/extra/For_Loop_Gauntlet.java
+++ src/extra/For_Loop_Gauntlet.java
@@ -1,64 +1,61 @@
 package extra;
 
 public class For_Loop_Gauntlet {
-public static void main(String[] args) {
-	//1:		for(int a = 0; a < 100; a++) {
-	//System.out.println(a);}
-	
-	//2:		for(int b = 100; b > 0; b--) {
-	//System.out.println(b);}
-	
-	//3.		for(int c = 2; c < 100;c++) {
-	//if(c%2==0) {
-	//System.out.println(c);}}
-	
-	//4: for(int d = 0; d < 100; d++) {
-	//if(d%2==1) {
-	//System.out.println(d);}}
-	
-	//5.		for(int d = 1; d < 500; d++) {
-	//if(d%2==1) {
-	//System.out.println(d + " is odd");}
-	//if(d%2==0) {
-	//System.out.println(d + " is even");}}
-	
-	//6.		for (int e = 0; e < 777; e++) {
-	//if(e%7==0) {
-	//System.out.println(e);}}
-		
-	
-	//7.		for (int f = 2007; f < 2018; f++) {
-	//int g = f-2007;
-	//System.out.println("In " + f + " I was " + g);}
-		
-	
-		
-	//1. for(int h = 0; h < 3; h++ ) {
-		
-		//for (int i = 0; i < 3; i++) {
-		//System.out.println(h + " " + i);}}
-		
-	//2. for (int j = 1; j < 9; j+=3) {
-		
-		//for (int k = j; k < j+3; k++) {
-			//System.out.print(k);}
-		
-		//System.out.println();}
-	
-		//3. for (int l = 1; l < 100; l+=10) {
-		
-		//for (int m = l; m < l+10; m++) {
-		//System.out.print(m);}
-		
-		//System.out.println();}
-		
+	public static void main(String[] args) {
+		// 1: for(int a = 0; a < 100; a++) {
+		// System.out.println(a);}
+
+		// 2: for(int b = 100; b > 0; b--) {
+		// System.out.println(b);}
+
+		// 3. for(int c = 2; c < 100;c++) {
+		// if(c%2==0) {
+		// System.out.println(c);}}
+
+		// 4: for(int d = 0; d < 100; d++) {
+		// if(d%2==1) {
+		// System.out.println(d);}}
+
+		// 5. for(int d = 1; d < 500; d++) {
+		// if(d%2==1) {
+		// System.out.println(d + " is odd");}
+		// if(d%2==0) {
+		// System.out.println(d + " is even");}}
+
+		// 6. for (int e = 0; e < 777; e++) {
+		// if(e%7==0) {
+		// System.out.println(e);}}
+
+		// 7. for (int f = 2007; f < 2018; f++) {
+		// int g = f-2007;
+		// System.out.println("In " + f + " I was " + g);}
+
+		// 1. for(int h = 0; h < 3; h++ ) {
+
+		// for (int i = 0; i < 3; i++) {
+		// System.out.println(h + " " + i);}}
+
+		// 2. for (int j = 1; j < 9; j+=3) {
+
+		// for (int k = j; k < j+3; k++) {
+		// System.out.print(k);}
+
+		// System.out.println();}
+
+		// 3. for (int l = 1; l < 100; l+=10) {
+
+		// for (int m = l; m < l+10; m++) {
+		// System.out.print(m);}
+
+		// System.out.println();}
+
 		for (int n = 0; n < 6; n++) {
-			
-		for (int o = n; o < n+; o++) {
-			System.out.print("*");
-		}
-		System.out.println();
+
+			for (int o = n; o < n + 3; o++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 
-}
+	}
 }
